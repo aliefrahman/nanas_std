daad5919025c96692eb8fa8bc20abd6df9554b63
teknodaeng <teknodaeng@gmail.com>
2026-06-05 03:37:48 +0800

    footer fixed

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto px-6 space-y-6">
         <p class="text-xs text-slate-700">&copy;
             <?= date('Y') ?>
-            <?= htmlspecialchars($companyName) ?> | <?= htmlspecialchars($tagline) ?>.
+            <?= htmlspecialchars($companyName) ?> -- <?= htmlspecialchars($tagline) ?>.
         </p>
 
     </div>
